9f1937efe558c2c67e9bc5536a3ca0fa99593e29
rasulov998 <rasulovsahboz661@gmail.com>
2026-09-07 09:25:10 +0300

    Add Supabase client config

---
+++ supabase-client.js
@@ -0,0 +1,3 @@
+window.VIBE_SUPABASE_URL='https://tfznfexkuffttopmgtms.supabase.co';
+window.VIBE_SUPABASE_KEY='sb_publishable_WKnPp5gYxLYL5uNX7N7rRQ_DUJqYMY9';
+window.vibeSupabase=window.supabase.createClient(window.VIBE_SUPABASE_URL,window.VIBE_SUPABASE_KEY);
